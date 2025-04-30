e34b2b33615c26f1d57214a9f9e485d23404e75f
ki-ELXR <128264877+ki-ELXR@users.noreply.github.com>
2025-04-30 10:21:13 +0000

    Deploying to gh-pages from @ ki-ELXR/ki-elxr.github.io@e462ca280280d0d422b8402f467f9f7c57a0e80c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,47 +402,52 @@ ninja.data = [{
           title: 'Islamic Patterns',
           description: "A implementation in C++ and Web Assembly",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/101_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/2_project/";
+              window.location.href = "/projects/102_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/3_project/";
+              window.location.href = "/projects/103_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/4_project/";
+              window.location.href = "/projects/104_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/5_project/";
+              window.location.href = "/projects/105_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/6_project/";
+              window.location.href = "/projects/106_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/7_project/";
+              window.location.href = "/projects/107_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/8_project/";
+              window.location.href = "/projects/108_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/unused_projects/9_project/";
+              window.location.href = "/projects/109_project/";
+            },},{id: "projects-islamic-patterns",
+          title: 'Islamic Patterns',
+          description: "A implementation in C++ and Web Assembly",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
             },},{
         id: 'social-email',
         title: 'email',
